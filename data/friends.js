ef608b7585630561cafd3d442bab89f46bfe65e8
avillalobosd <lalovide@gmail.com>
2018-07-19 17:28:39 -0500

    final

---
+++ data/friends.js
@@ -0,0 +1,38 @@
+var tablefriends = [
+    {
+        "name":"Robert",
+        "photo":"https://daks2k3a4ib2z.cloudfront.net/55d62f32fa59c51977889877/561d4d3b8cf0398714ac71b5_MM-092714_Avatar.jpg",
+        "scores":[
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+          ]
+    },
+    {
+        "name":"Fiona",
+        "photo":"http://www.bistiproofpage.com/wp-content/uploads/2018/04/cute-profile-pics-for-whatsapp-images.png",
+        "scores":[
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2"
+          ]
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = tablefriends;
+  
